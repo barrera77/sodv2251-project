f169f07b1b4fdf2de4f13b38893a4dee75ef3b30
barrera77 <barrera_ml@hotmail.com>
2025-05-18 00:06:33 -0600

    Project setup including dummy screens

---
+++ app/(tabs)/signup/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <Text>Signup Page</Text>
+    </View>
+  );
+};
+
+export default SignupScreen;
